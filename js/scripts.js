502f5a62b328c2185b25c52fd9befe77db4eef5d
Muharem Sumic <muharem22@gmail.com>
2020-11-28 11:14:20 -0600

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,4 @@
-//   <script>
-//   $(function () {
-//     $('[data-toggle="tooltip"]').tooltip();
-// });
-
-
-//     jquery scrip for carousel play/pause buttons code for the previous play/pause button -->
-
-// $(function() {
-//     $(".carousel").carousel( { interval: 2000, pause: "false"  } );  //The pause: "falese" eliminates issue in small devices
-//     $("#carouselPause").click(function(){
-//         $(".carousel").carousel("pause");
-//     });
-//     $("#carouselPlay").click(function(){
-//         $(".carousel").carousel("cycle");
-//     });
-// });
-
-
-// script for only single pause/play button to cycle to change when clicked
-
-
+// Carousel function
 $(function() {
     $(".carousel").carousel( { interval: 2000 } );
     $("#carouselButton").click(function(){
@@ -35,3 +14,19 @@ $(function() {
     });
 });
 
+//when reserveCampsite button is clicked reserveCampsite modal is called
+//task2
+$(function(){
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal();
+        
+    });
+});
+
+//when login button is clicked login modal is called
+$(function(){
+    $("#loginButton").click(function(){
+        $("#loginModal").modal();
+        
+    });
+});
